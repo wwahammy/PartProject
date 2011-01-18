8d99b94d99b2fca70e0b1b07658935521134b5e6
Eric Schultz <wwahammy@gmail.com>
2011-01-18 09:42:19 -0600

    Added the rename/decompression of results script

---
+++ scripts/ren-dec.rb
@@ -0,0 +1,18 @@
+=begin
+Renames and decompresses results files
+-------
+The result files are compressed using gzip. In order to unzip them, we
+rename them by adding .gz to the end of the filename and then run the 
+gzip on the file.
+=end
+
+files = File.join("**", "*")
+Dir.glob(files) { |filename|
+	unless (File.directory?(filename) || File.stat(filename).size == 100000012 || filename =~ /.*(\.rb|\.gz)/)
+		name = File.expand_path ("./#{filename}")
+		File.rename(name, "#{name}.gz")
+		system("gzip -d #{name}.gz")
+		puts name
+	end
+}
+
